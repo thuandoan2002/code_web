e9d29f4e67ddcdb80072661e42c2ad21e931d101
Vivian Doan <dqhoa2@cmcglobal.vn>
2023-02-06 23:32:17 +0700

    commit

---
+++ src/main/java/cmcglobal/vn/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package cmcglobal.vn.repository;
+
+import cmcglobal.vn.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/cmcglobal/vn/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package cmcglobal.vn.web.rest.vm;
